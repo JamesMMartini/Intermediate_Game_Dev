954f00f037b955af51ce8d961082016726ee293f
James Martini <jamesmartini1008@gmail.com>
2021-10-23 12:43:25 -0400

    added the ending

---
+++ Test 2D Project/Assets/Treasure Hunt/Scripts/EndFriend.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class EndFriend : MonoBehaviour
+{
+    public TMP_Text questText;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.name == "Player")
+        {
+            StartCoroutine(DisplayEnd());
+        }
+    }
+
+    private IEnumerator DisplayEnd()
+    {
+        questText.text = "Congratulations!";
+        questText.enabled = true;
+
+        yield return new WaitForSeconds(3f);
+
+        questText.enabled = false;
+    }
+}
